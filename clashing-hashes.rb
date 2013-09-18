188759f30da42fbb5d05177b241d7e05f1200ad8
Michael Ornellas <mike@themakersquare.com>
2013-09-18 14:40:17 -0500

    adding in twitter

--- clashing-hashes.rb
+++ clashing-hashes.rb
@@ -2,7 +2,18 @@ require 'rubygems'
 require 'bundler/setup'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'twitter'
+
+# require_relative './twitter'
 
 get '/' do
-  'hello world'
+  @name = "Mike"
+  @show_description = true
+  erb :index
 end
+
+post '/twitter' do
+  Twitter.
+  erb :results
+end
+
