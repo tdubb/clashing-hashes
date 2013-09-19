b6dfb00c7d451ec4907bc7ca67ab0fdadd5df0d2
Shehzan Devani <shehzandevani@gmail.com>
2013-09-19 10:29:52 -0500

    Live coding solutions

--- clashing-hashes.rb
+++ clashing-hashes.rb
@@ -2,7 +2,21 @@ require 'rubygems'
 require 'bundler/setup'
 require 'sinatra'
 require 'sinatra/reloader'
+require_relative 'lib/twitter'
+
+include Twitter
 
 get '/' do
-  'hello world'
+  @name = "Shehzan"
+  @show_description = false
+  erb :hello
+end
+
+
+post '/twitter' do
+  @tweets = Twitter::Tweet.give_me_tweets(20)
+  @first = params[:first]
+  @second = params[:second]
+
+  erb :twitter_results
 end
